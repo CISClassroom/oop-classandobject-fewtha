aaceee116762ea8917792c4e882aa40c0bd30299
fewtha <59079336+fewtha@users.noreply.github.com>
2020-03-08 20:02:55 +0700

    fewtha-623410034-7

---
+++ src/com/company/IronManCF.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class IronManCF {
+    public class IronManRedGrey
+    {
+        String model;
+        String color;
+        String manufacturing_company;
+        String Mask;
+        void CanMoveLimes(){}
+        void OpenMask(){}
+        void Show(){}
+        void Playable(){}
+    }
+    class IronManGrey
+    {
+        String model;
+        String color;
+        String manufacturing_company;
+        String Mask;
+        void CanMoveLimes(){}
+        void OpenMask(){}
+        void Show(){}
+        void Playable(){}
+    }
+    class IronManBlackGold
+    {
+        String model;
+        String color;
+        String manufacturing_company;
+        String Mask;
+        void CanMoveLimes(){}
+        void OpenMask(){}
+        void Show(){}
+        void Playable(){}
+    }
+    class IronManRed
+    {
+        String model;
+        String color;
+        String manufacturing_company;
+        String Mask;
+        void CanMoveLimes(){}
+        void OpenMask(){}
+        void Show(){}
+        void Playable(){}
+    }
+
+}
---
+++ src/com/company/TreeObjectCF.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class TreeObjectCF {
+    public class TreeCF
+    {
+        String name;
+        String age;
+        String Use;
+        String GardenStyle;
+        void photosynthesis(){}
+        void SpitoutOxygen(){}
+        void Shade(){}
+        void AbsorbCarbondioxide(){}
+    }
+
+}
